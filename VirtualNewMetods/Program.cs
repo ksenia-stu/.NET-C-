540779ff8b4fc7406095491e0085d823a5417586
KseniaStu <studilina.ksenia@gmail.com>
2021-01-28 12:21:11 -0500

    inheritance

---
+++ VirtualNewMetods/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualNewMetods
+{
+    class Program
+    {
+        class Animal
+        {
+            public virtual void Introduce()
+            {
+                Console.WriteLine("I am an animal");
+            }
+        }
+
+        class Cabra : Animal
+        {
+            public override void Introduce()
+            {
+                Console.WriteLine("I am a cabra");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Introduce();
+
+            Cabra caba = new Cabra();
+            caba.Introduce();
+
+
+            Console.WriteLine("-----------------");
+
+            animal = caba;
+            animal.Introduce();
+
+
+            Console.ReadKey();
+        }
+    
+    }
+}
