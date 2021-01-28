418d4df57790e872fde7d7ad354f6877d29f9eb5
KseniaStu <studilina.ksenia@gmail.com>
2021-01-28 12:18:42 -0500

    generate prime numbers using indexer

---
+++ Day18Indexer/PrimeArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18Indexer
+{
+    class PrimeArray
+    {
+        private bool IsPrime(long number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+
+
+       
+
+// indexer
+public long this[int index]
+        {
+            get
+            {
+                long number = IsPrime(index)? 
+                return IsPrime(index);
+            }
+            
+        }
+    }
+}
---
+++ Day18Indexer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeArray pa = new PrimeArray();
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i + " : " + pa[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
