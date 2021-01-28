b3ab0cc185228039a11b0e93e990a231385ef702
KseniaStu <studilina.ksenia@gmail.com>
2021-01-28 12:13:32 -0500

    custom dialog

---
+++ Day07Sandwich(final)/CustomDialog.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Day07Sandwich_final_
+{
+    /// <summary>
+    /// Interaction logic for CustomDialog.xaml
+    /// </summary>
+    public partial class CustomDialog : Window
+    {
+        public event Action<string, string, string> AssignResult;
+
+        public CustomDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButtonClicked(object sender, RoutedEventArgs e)
+        {
+            //bread
+            string bread = comboBread.Text;
+            
+            //veggies
+            List<string> veggies = new List<string>();
+            if(cbVegLettuce.IsChecked == true)
+            {
+                veggies.Add("Lettuce");
+            }
+            if (cbVegTomatoes.IsChecked == true)
+            {
+                veggies.Add("Tomatoes");
+            }
+            if (cbVegCucubmers.IsChecked == true)
+            {
+                veggies.Add("Cucumbers");
+            }
+
+            string vegs = string.Join(",", veggies);
+
+            //meat
+            string meat = "";
+            if (rbMeatChicken.IsChecked == true)
+            {
+                meat = "Chicken";
+            }
+            else if (rbMeatTurkey.IsChecked == true)
+            {
+                meat = "Turkey";
+            }
+            else if (rbMeatTofu.IsChecked == true)
+            {
+                meat = "Tofu";
+            }
+            else
+            {
+                MessageBox.Show("Please choose meat", "Action required", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            AssignResult?.Invoke(bread, vegs, meat);
+         /*   if (AssignResult != null)
+            {
+                AssignResult.Invoke(bread, vegs, meat);
+            } */
+            DialogResult = true;
+        }
+    }
+}
---
+++ Day07Sandwich(final)/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day07Sandwich_final_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //we can create separate method INSTEAD OF LAMBDA
+        private void CustDlg_AssignResult(string bread, string veg, string meat)
+        {
+            lblBread.Content = bread;
+            lblVeggies.Content = veg;
+            lblMeat.Content = meat;
+        }
+
+        private void MakeSandwichButtonClicked(object sender, RoutedEventArgs e)
+        {
+          /*  string breadVal = "";
+            string veggiesVal = "";
+            string meatVal = ""; */
+
+            CustomDialog cd = new CustomDialog();
+            cd.Owner = this;
+            // cd.AssignResult += (b, v, m) => { breadVal = b; veggiesVal = v; meatVal = m; };
+            cd.AssignResult += CustDlg_AssignResult; //alternative to lambda
+
+            bool? result = cd.ShowDialog();  // cd.ShowDialog() not null then show it and save result to bool(true/false)
+
+         /*   if(result == true)
+            {
+                lblBread.Content = breadVal;
+                lblVeggies.Content = veggiesVal;
+                lblMeat.Content = meatVal;
+            } */
+        }
+    }
+}
