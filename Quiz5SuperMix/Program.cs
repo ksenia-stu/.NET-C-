d0000ee9a3a232d03beb87dbc8d1feeb7f4d1bde
KseniaStu <studilina.ksenia@gmail.com>
2021-01-28 12:19:40 -0500

    quiz

---
+++ Quiz5SuperMix/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz5SuperMix
+{
+    class Program
+    {
+        public const int MaxValue = 2147483647;
+        static void Main(string[] args)
+        {
+            SuperFibs sf = new SuperFibs();
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i + " : " + sf[i]);
+            }
+            try
+            {
+                Console.WriteLine(sf[0]);
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            Console.WriteLine("1: {0}, steps {1}", sf[1], sf.StepsCount); // prints 1: 0, steps 0
+            Console.WriteLine("2: {0}, steps {1}", sf[2], sf.StepsCount); // prints 2: 1, steps 0
+            Console.WriteLine("3: {0}, steps {1}", sf[3], sf.StepsCount); // prints 3: 1, steps 0
+            Console.WriteLine("5: {0}, steps {1}", sf[5], sf.StepsCount); // prints 5: 4, steps 2
+            Console.WriteLine("9: {0}, steps {1}", sf[9], sf.StepsCount); // prints 9: 44, steps 4
+            Console.WriteLine("7: {0}, steps {1}", sf[7], sf.StepsCount); // prints 7: 13, steps 0 (because it's already in the cache) 
+            Console.WriteLine("9: {0}, steps {1}", sf[9], sf.StepsCount);
+
+            //Prompt user to enter seconds
+            float userTime;
+            while (true)
+            {
+                Console.Write("Please enter number of seconds: ");
+                
+                try
+                {
+                    userTime = float.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Enter numbers only");
+                }
+            }
+
+            var startTime = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds; //starting time point
+
+
+            for (int i = 1; i < MaxValue; i++)
+            {
+               Console.WriteLine(i + " : " + sf[i]);
+                if(((int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds - startTime) > userTime)
+                {
+                    Console.WriteLine(i + " numbers are generated in " + ((int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds - startTime) + " second(s)");
+                    break;
+                }
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Quiz5SuperMix/SuperFibs.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz5SuperMix
+{
+    public class SuperFibs
+    {
+        private Dictionary<int, long> cache = new Dictionary<int, long> { { 1, 0 }, { 2, 1 }, { 3, 1 } };
+        public int StepsCount;
+
+
+        private long getNthFib(int n)
+        {
+
+
+            if (cache.ContainsKey(n))
+            {
+                return cache[n];
+            }
+            else
+            {
+                
+                cache[n] = getNthFib(n - 1) + getNthFib(n - 2) + getNthFib(n - 3);
+                StepsCount++;
+                return (getNthFib(n - 1) + getNthFib(n - 2) + getNthFib(n - 3));
+                
+            }
+
+        //without cache
+        /*    if (n == 1)
+            {
+                return 0;
+            }
+            else if (n == 2)
+            {
+                return 1;
+            }
+            else if( n == 3)
+            {
+                return 1;
+            }
+            else
+            {
+                return getNthFib(n - 1) + getNthFib(n - 2) + getNthFib(n -3);
+            } */
+        }
+
+        // indexer
+        public long this[int index]
+        {
+            get
+            {
+                if (index < 1)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else
+                {
+                    return getNthFib(index);
+                }
+            }
+
+        }
+    }
+}
